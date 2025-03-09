cc43dfca7718227c02c3d021f0df31efe03b73bc
Ali Alnemary <alialnemary-23@hotmail.com>
2025-03-09 22:05:27 +0300

    set up the templates and DB

--- Planteer/main/views.py
+++ Planteer/main/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from .models import Plant
 # Create your views here.
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def plant(request):
+    return render(request, 'main/plant.html')
+
+def detail(request):
+    return render(request, 'main/detail.html')
+
+def add(request):
+    return render(request, 'main/add.html')
+
+def update(request):
+    return render(request, 'main/update.html')
+
+def delete(request):
+    return render(request, 'main/delete.html')
+
+def search(request):
+    return render(request, 'main/search.html')
