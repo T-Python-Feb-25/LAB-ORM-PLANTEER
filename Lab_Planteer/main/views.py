a7f3b7136436dd2a74efbf9bd2992629085935ed
Ghaida <ghaidagh347@gmail.com>
2025-03-10 22:33:03 +0300

    lab

---
+++ Lab_Planteer/main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+# Create your views here.
+
+
+def Home_view(request: HttpRequest):
+    return render(request, "main/index.html")
---
+++ Lab_Planteer/planteer/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlanteerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'planteer'
